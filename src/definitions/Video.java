49354c22e3afc0dc8c43464c7627c717421907f1
KvChahar <krishnaveer.chahar_cs19@gla.ac.in>
2020-08-21 18:00:22 +0530

    add a method to get the name of the video

--- src/definitions/Video.java
+++ src/definitions/Video.java
@@ -1,7 +1,27 @@
 package definitions;
 
 public class Video {
+    /*
+     * Creating Definition Classes: (Rules)
+     * 1. Always set the visibility modifier of a field to private unless otherwise specified.
+     * 2. Always set the access modifier of a field to non-static unless otherwise specified.
+     **/
+
+    // member variables ==> fields / variables
     private String videoName;
     private boolean checkOut;
     private int rating;
+
+    // member functions ==> methods / functions
+
+    /**
+     * *This method returns the name of the video.
+     *
+     * @return The name of the video
+     */
+
+    public String getName() {
+        return "";
+
+    }
 }
